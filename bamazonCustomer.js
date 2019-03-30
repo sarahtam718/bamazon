c4258bbbacee6ce3af2ff1565bb0574d222b09fa
Sarah Tam <sarahtam718@gmail.com>
2019-03-30 09:12:37 -0500

    inserted js file with pseudocode

---
+++ bamazonCustomer.js
@@ -0,0 +1,21 @@
+
+// in mysql...
+
+    // create database [DONE]
+
+    // create table [DONE]
+
+    // insert data
+
+// in js...
+
+    // require packages
+        // require mysql & inquirer
+
+    // connect to mysql table (see documentation)
+
+    // show products from table using query
+
+    // run inquirer with list
+        // if else statement depending on if enough quantity for user to purchase
+        // if enough, update table using query & show products again using query
