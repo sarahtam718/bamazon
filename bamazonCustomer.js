a4e45b477b48d8e9ad690916e60081ee2f660f20
Sarah Tam <sarahtam718@gmail.com>
2019-03-30 10:32:48 -0500

    basics of purchase query working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,21 +30,60 @@ var connection = mysql.createConnection({
 });
 
 
-// // show products from table using query
+// // show products from table
 function showProducts() {
-    connection.query("select * from products;", function (err, res) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(res);
-            connection.end();
+    connection.connect(function (err) {
+        if (err) throw err;
+        // console.log("connected as id " + connection.threadId);
+        for (let i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | Product: " + res[i].product_name + " | Department: " + res[i].department_name + " | Price: " + res[i].price);
         }
+        connection.end();
     });
 }
 
-showProducts();
+// showProducts();
 
+// ask user for what they want to buy and how much
+inquirer.prompt([
+    {
+        type: "input",
+        name: "toBuy",
+        message: "What is the ID number of the product you wish to purchase?",
+    },
+    {
+        type: "input",
+        name: "amount",
+        message: "How many units of the product do you wish to purchase?",
+    }
+]).then(ans => {
+    // console.log("user input: ", ans);
+    console.log("Checking our inventory...");
+    // if else statement depending on if enough quantity for user to purchase
+    connection.query(`SELECT * FROM products WHERE item_id ='${ans.toBuy}'`, function (err, data) {
+        // console.log(data);
+        for (let j = 0; j < data.length; j++) {
+            // console.log("\nhow much do we have? ", data[j].stock_quantity, "\n how much does the customer want? ", ans.amount);
+            if (err) {
+                console.log(err);
+            } else if (data[j].stock_quantity < ans.amount) {
+                console.log("Our apologies, but we have an insufficient quantity of that product.")
+                //     // start();
+            } else {
+                console.log(`You have purchased ${ans.amount} unit(s) of this product: ${data[j].product_name}.`)
+                //         // updateDB();
+                //         // showProducts();
+                //         // total()
+            }
+        }
 
-    // run inquirer with list
-        // if else statement depending on if enough quantity for user to purchase
-        // if enough, update table using query & show products again using query
+        connection.end()
+    });
+
+});
+
+// start function with show products () & inquirer ()
+
+// updateDB () that removes quantity from stock
+
+// total() = stock_quantity - ans.amount x price
