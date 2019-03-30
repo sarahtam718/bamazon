4c3fb40b8bbc2949049b548e794d40beb2a60768
Sarah Tam <sarahtam718@gmail.com>
2019-03-30 09:41:42 -0500

    installed inquirer & mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,49 @@
 
 // in mysql...
 
-    // create database [DONE]
+// create database [DONE]
 
-    // create table [DONE]
+// create table [DONE]
 
-    // insert data
+// insert data [DONE]
 
 // in js...
 
-    // require packages
-        // require mysql & inquirer
+// require packages
+// require mysql & inquirer
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-    // connect to mysql table (see documentation)
+// connect to mysql table (see documentation)
+// var connection = mysql.createConnection({
+//     host: "localhost",
 
-    // show products from table using query
+//     // Your port; if not 3306
+//     port: 3307,
+
+//     // Your username
+//     user: "root",
+
+//     // Your password
+//     password: "root",
+//     database: "bamazon"
+// });
+
+
+// // show products from table using query
+// function showProducts() {
+//     connection.query("select * from products;", function (err, res) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(res);
+//             connection.end();
+//         }
+//     });
+// }
+
+// showProducts();
+
 
     // run inquirer with list
         // if else statement depending on if enough quantity for user to purchase
